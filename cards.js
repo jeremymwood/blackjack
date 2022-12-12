27a4c9f56650196a475b32b4102c3b133f57b5d5
Jeremy Wood <jeremy.mason.wood@gmail.com>
2022-12-12 14:42:09 -0600

    221212-1442

--- cards.js
+++ cards.js
@@ -1,16 +1,136 @@
 `use strict`;
+//http://www.brainjar.com/js/blackjack/
 //each card an object with suit and rank
 
-const suit = [`diamonds`,`clubs`,`hearts`,`spades`];
-const rank = [`ace`,`king`,`queen`,`jack`,`10`,`9`,`8`,`7`,`6`,`5`,`4`,`3`,`2`];
-
-const card = {
-    suit,
-    rank
-}
-
-//creates object containing all suits and ranks
+// const suit = [`diamonds`,`clubs`,`hearts`,`spades`];
+// const rank = [`ace`,`king`,`queen`,`jack`,`10`,`9`,`8`,`7`,`6`,`5`,`4`,`3`,`2`];
+//
+// // const cards = {
+// //
+// // }
+//
+// // cards.forEach()
+//
+// function createCards(){
+//      for (const card in cards) {
+//         console.log(`${rank} of ${suit}`);
+//     }
+// }
+// // createCards(cards);
+//
+//
+// // creates object containing all suits and ranks
+// const card = {
+//     suit,
+//     rank
+// }
+//
 // for (const property in card) {
 //     console.log(`${property}: ${card[property]}`);
 // }
-// console.log(card);
+// console.log(card.toString());
+
+let suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+let deck = new Array();
+
+function createDeck()
+{
+    deck = new Array();
+    for (let i = 0 ; i < values.length; i++)
+    {
+        for(let x = 0; x < suits.length; x++)
+        {
+            let weight = parseInt(values[i]);
+            if (values[i] == "J" || values[i] == "Q" || values[i] == "K")
+                weight = 10;
+            if (values[i] == "A")
+                weight = 11;
+            let card = { Value: values[i], Suit: suits[x], Weight: weight };
+            deck.push(card);
+        }
+    }
+}
+
+function shuffle()
+{
+    // for 1000 turns
+    // switch the values of two random cards
+    for (let i = 0; i < 1000; i++)
+    {
+        let location1 = Math.floor((Math.random() * deck.length));
+        let location2 = Math.floor((Math.random() * deck.length));
+        let tmp = deck[location1];
+
+        deck[location1] = deck[location2];
+        deck[location2] = tmp;
+    }
+}
+
+let players = new Array();
+function createPlayers(num)
+{
+    players = new Array();
+    for(let i = 1; i <= num; i++)
+    {
+        let hand = new Array();
+        let player = { Name: 'Player ' + i, ID: i, Points: 0, Hand: hand };
+        players.push(player);
+    }
+}
+
+function createPlayersUI()
+{
+    document.getElementById('players').innerHTML = '';
+    for(var i = 0; i < players.length; i++)
+    {
+        var div_player = document.createElement('div');
+        var div_playerid = document.createElement('div');
+        var div_hand = document.createElement('div');
+        var div_points = document.createElement('div');
+
+        div_points.className = 'points';
+        div_points.id = 'points_' + i;
+        div_player.id = 'player_' + i;
+        div_player.className = 'player';
+        div_hand.id = 'hand_' + i;
+
+        div_playerid.innerHTML = players[i].ID;
+        div_player.appendChild(div_playerid);
+        div_player.appendChild(div_hand);
+        div_player.appendChild(div_points);
+        document.getElementById('players').appendChild(div_player);
+    }
+}
+
+function startblackjack()
+{
+    document.getElementById('btnStart').value = 'Restart';
+    document.getElementById("status").style.display="none";
+    // deal 2 cards to every player object
+    currentPlayer = 0;
+    createDeck();
+    shuffle();
+    createPlayers(2);
+    createPlayersUI();
+    dealHands();
+    document.getElementById('player_' + currentPlayer).classList.add('active');
+}
+
+function dealHands()
+{
+    // alternate handing cards to each player
+    // 2 cards each
+    for(var i = 0; i < 2; i++)
+    {
+        for (var x = 0; x < players.length; x++)
+        {
+            var card = deck.pop();
+            players[x].Hand.push(card);
+            renderCard(card, x);
+            updatePoints();
+        }
+    }
+
+    updateDeck();
+}
